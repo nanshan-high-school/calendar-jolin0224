da014aaa38ebf41c78024b0c883c454bea4727a5
jolin0224 <32165432+jolin0224@users.noreply.github.com>
2018-04-12 18:53:03 +0800

    Update gistfile1.cpp

--- gistfile1.cpp
+++ gistfile1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
     int i, j, k;
     cout << "請輸入餅乾 巧克力 蛋糕的數量\n";
     cin >> i >> j >> k;
     
-    if(i >= 10 && k >= 2){
-        cout << i << "個餅乾 " << j + 1 << "盒巧克力 " << k << "個蛋糕";
+    while(i >= 10 && k >= 2){
+        j++;
+        i -= 10;
+        k -= 2;
     }
-
+    cout << i << "個餅乾 " << j << "盒巧克力 " << k << "個蛋糕\n";
     return 0;
 }
